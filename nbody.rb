40d5001b1948850d6eda7a50f41872394eab0bdc
ben-urbana <benurbana1@gmail.com>
2017-12-18 21:32:18 -0700

    fixed additive forces

--- nbody.rb
+++ nbody.rb
@@ -6,50 +6,50 @@ require "./simulation"
 
 class NbodySimulation < Gosu::Window
 
-	def initialize
-    	super(640, 640, false)
-    	
-    	@background_image = Gosu::Image.new("images/space.jpg", tileable: true)
-    	self.read("planets.txt")
+  def initialize
+      super(640, 640, false)
+      
+      @background_image = Gosu::Image.new("images/space.jpg", tileable: true)
+      self.read("planets.txt")
     end
 
-  	def read(file)
-  		bodies = []
-  		amount_of_bodies = 0
-  		radius = 0
-		File.open("./simulations/#{file}") do |a|
-			a.each_line.with_index do |line, i|
-				if i == 0
-					amount_of_bodies = line
-				elsif i == 1
-					@radius = line
-				else
-					pieces = line.gsub(/\s+/m, ' ').strip.split(" ")
-					if pieces[0] == nil
-						next
-					end
-					if pieces[0] == "Creator"
-						break
-					end
-					bodies.push(Body.new(pieces[0].to_f, pieces[1].to_f, pieces[2], pieces[3], pieces[4], pieces[5]))
-				end
-			end
-		end
-		@simulation = Simulation.new(amount_of_bodies, radius, bodies)
-	end
+    def read(file)
+      bodies = []
+      amount_of_bodies = 0
+      radius = 0
+    File.open("./simulations/#{file}") do |a|
+      a.each_line.with_index do |line, i|
+        if i == 0
+          amount_of_bodies = line
+        elsif i == 1
+          radius = line.to_f
+        else
+          pieces = line.gsub(/\s+/m, ' ').strip.split(" ")
+          if pieces[0] == nil
+            next
+          end
+          if pieces[0] == "Creator"
+            break
+          end
+          bodies.push(Body.new(pieces[0].to_f, pieces[1].to_f, pieces[2].to_f, pieces[3].to_f, pieces[4].to_f, pieces[5]))
+        end
+      end
+    end
+    @simulation = Simulation.new(amount_of_bodies, radius, bodies)
+  end
 
-	def update
-  		@simulation.update
-  	end
+  def update
+      @simulation.update
+    end
 
-  	def draw
-    	@background_image.draw(0, 0, ZOrder::Background)
-    	@simulation.draw
-  	end
+    def draw
+      @background_image.draw(0, 0, ZOrder::Background)
+      @simulation.draw
+    end
 
-  	def button_down(id)
-    	close if id == Gosu::KbEscape
-  	end
+    def button_down(id)
+      close if id == Gosu::KbEscape
+    end
 end
 
 window = NbodySimulation.new
