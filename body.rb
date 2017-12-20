07c7c638a398f38feb8a91b02451e10a7ab5e7bd
ben-urbana <benurbana1@gmail.com>
2017-12-20 06:58:58 -0700

    Fixed gravitation error

--- body.rb
+++ body.rb
@@ -2,28 +2,28 @@ require "gosu"
 
 class Body
 
-	attr_accessor :x, :y, :vel_x, :vel_y, :mass, :F, :ax, :ay, :x_coord, :y_coord
+	DT = 25000
+	G = 6.67408E-11
 
-	def initialize(x, y, vel_x, vel_y, mass, image)
+	attr_accessor :x, :y, :vel_x0, :vel_y0, :mass, :F, :ax, :ay, :x_coord, :y_coord
+
+	def initialize(x, y, vel_x0, vel_y0, mass, image)
 		@x = x.to_f
 		@y = y.to_f
-		@vel_x = vel_x.to_f
-		@vel_y = vel_y.to_f
+		@vel_x = vel_x0.to_f
+		@vel_y = vel_y0.to_f
 		@mass = mass.to_f
 		@image = Gosu::Image.new("images/#{image}", tileable: true)
+		@pic = image
 		@F = 0.0
-		@vel_y0 = 0.0
-		@vel_x0 = 0.0
 		@Fx = 0.0
 		@Fy = 0.0
+
 	end
 
 
 
 	def compare(bodies)
-
-		@time = 25000
-		@grav = 6.67408 * 10e-11
 
 		bodies.each do |other_body|
 			next if other_body == self
@@ -31,32 +31,46 @@ class Body
 			@dy = other_body.y - @y
 			@r = Math.sqrt(@dx ** 2 + @dy ** 2)
 
-			@F = (@grav * @mass * other_body.mass) / (@r ** 2)
-			@Fx += @F * (@dx / @r)
-			@Fy += @F * (@dy / @r)
-			@vel_y0 = @vel_y
-			@vel_x0 = @vel_x
-			@ax = @Fx / @mass
-			@ay = @Fy / @mass
+			@F = (G * @mass * other_body.mass) / (@r ** 2)
+			@Fx += @F * @dx / @r
+			@Fy += @F * @dy / @r
 			
-			@vel_y = @ay * @time + @vel_y0
-			@vel_x = @ax * @time + @vel_x0
-
-			@Fx = 0.0
-			@Fy = 0.0
-
-			@x += @vel_x * @time + @vel_x0
-			@y += @vel_y * @time + @vel_y0
-
-						
-
+			
+			
 		end
+		move
 	end
 
+	def move
+		@ax = @Fx / @mass
+		@ay = @Fy / @mass
+
+		@vel_y += @ay * DT
+		@vel_x += @ax * DT
+
+		
+
+
+		
+
+		@Fx = 0.0
+		@Fy = 0.0
+		
+
+		@x += @vel_x * DT
+		@y += @vel_y * DT
+
+		
+
+		
+	end
+
+
 	def draw(radius)
-		@x_coord = 320 * @x / radius + 320 - (@image.width / 2)
-		@y_coord = 320 * @y / radius + 320 - (@image.height / 2)
+		@x_coord = 320 * @x / radius + 320 - (@image.width / 2.0)
+		@y_coord = -320 * @y / radius + 320 - (@image.height / 2.0)
 		@image.draw(@x_coord, @y_coord, 1)
+
 		# @x = @x_coord * 2.50e11 / 320 - 320
 		# @y = @y_coord * 2.50e11 / 320 - 320
 	end
